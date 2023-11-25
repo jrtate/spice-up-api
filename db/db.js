bec9bafec50169392b9031fa9b36955850223a78
jrtate <47283674+jrtate@users.noreply.github.com>
2023-11-24 19:28:05 -0500

    Fix db env

--- db/db.js
+++ db/db.js
@@ -3,20 +3,22 @@ import pg from "pg";
 // old pool
 const { Pool } = pg;
 
-const poolConfig = process.env.DATABASE_URL
-  ? {
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    }
-  : {
-      user: "postgres",
-      password: "postgres",
-      host: "localhost",
-      port: "5432",
-      database: "spiceup",
-    };
+const poolConfig =
+  process.env.NODE_ENV === "development"
+    ? {
+        user: "postgres",
+        password: "postgres",
+        host: "localhost",
+        port: "5432",
+        database: "spiceup",
+      }
+    : {
+        host: process.env.RDS_HOSTNAME,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        port: process.env.RDS_PORT,
+      };
 
 const pool = new Pool(poolConfig);
+
 export default pool;
